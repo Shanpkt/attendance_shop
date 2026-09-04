db08e028c5ae477f815bd6b346be391e234f77e3
Shantanu <shantanupathak01@gmail.com>
2026-09-04 15:37:56 +0530

    first commit

--- src/gps.jsx
+++ src/gps.jsx
@@ -50,7 +50,7 @@ function GPSLocation({ onLocationReady }) {
         // ACCURACY GOOD
         // ======================================
 
-        if (accuracy <= 150) {
+        if (accuracy <= 250) {
           const locationData = {
             latitude,
             longitude,
